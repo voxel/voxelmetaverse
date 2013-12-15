89eea21ae03dbd0edd4b40a875b900a5cc6c74b8
deathcap <deathcap@gmx.co.uk>
2013-12-14 17:26:21 -0800

    Change merge message

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 git checkout gh-pages
-git merge -X theirs master -m "Merge master"
+git merge -X theirs master -m "Merge branch 'master' into gh-pages"
 browserify test.coffee -t coffeeify > bundle.js
 git commit bundle.js -m "Regenerate bundle.js using deploy.sh"
 git checkout master
