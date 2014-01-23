8d4e092f78ee65fde4a445e3f6044191148fb822
deathcap <deathcap@gmx.co.uk>
2014-01-22 20:20:33 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 git checkout gh-pages
 git merge -X theirs master -m "Merge master"
-browserify test.coffee -t coffeeify > bundle.js
+browserify index.coffee -t coffeeify > bundle.js
 git commit bundle.js -m "Regenerate bundle.js"
 git checkout master
