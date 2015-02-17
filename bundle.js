dc95030347f7300703f7ad49e95fe1f5e816adb5
deathcap <deathcap@gmx.co.uk>
2015-02-16 19:22:17 -0800

    Regenerate bundle.js using deploy.sh

--- bundle.js
+++ bundle.js
@@ -1,4 +1,13 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+/**
+* 
+* latest commit: 2f82a08321a03b4bae952aab3af2605f269f8d81
+
+* created at: Mon Feb 16 2015 19:22:07 GMT-0800 (PST)
+* 
+**/
+
 var createEngine, main;
 
 require('voxel-artpacks');
