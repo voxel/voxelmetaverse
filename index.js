1e46b8e88e5b7d8369eadf641eb3495791391728
Rafi Khan <zetoslab@gmail.com>
2015-12-30 21:33:08 -0600

    added comments

--- index.js
+++ index.js
@@ -2,102 +2,105 @@
 (function() {
 
   // Plugins loaded by voxel-plugins are listed here for browserify
+  // Art Pack Selector Dialog
   require('voxel-artpacks');
-
+  // Shows wireframe if showWireFrame = true
   require('voxel-wireframe');
-
+  // Shows chunks around border (F9)
   require('voxel-chunkborder');
-
+  // Shows outline around targeted voxel
   require('voxel-outline');
-
+  // Player inventory
   require('voxel-carry');
-
+  // Buckets to pick up and place fluids
   require('voxel-bucket');
-
-  require('voxel-fluid');
-
-  require('voxel-skyhook');
-
+  // Fluid Registration
+  require('voxel-fluid'); // TODO: pending rename https://github.com/voxel/voxel-fluid/issues/2
+  // A Block which can be placed in the sky
+  require('voxel-skyhook'); // TODO: pending rename https://github.com/voxel/voxel-skyhook/issues/1
+  // Access to "craftingrecipes" (https://github.com/deathcap/craftingrecipes)
   require('voxel-recipes');
-
+  // Adds quarry block which can mine large blocks of ... well ... blocks
   require('voxel-quarry');
-
-  require('voxel-measure');
-
-  require('voxel-webview');
-
+  // Item that measures between blocks  
+  require('voxel-measure'); // TODO: pending rename https://github.com/voxel/voxel-measure/issues/1
+  // Not a whole lot of reason to keep this in the game... maybe a modified version
+  // which can fit within a GUI element
+  // require('voxel-webview');
+  // Renders scene for use with webvr
   require('voxel-vr');
-
-  require('voxel-workbench');
-
-  require('voxel-furnace');
-
-  require('voxel-chest');
-
-  require('voxel-inventory-hotbar');
-
+  // Adds 3x3 work bench (crafting table)
+  require('voxel-workbench'); // TODO: pending rename https://github.com/voxel/voxel-workbench/issues/4
+  // Adds furnace for smelting
+  require('voxel-furnace'); // TODO: pending rename https://github.com/voxel/voxel-furnace/issues/5
+  // Adds chest
+  require('voxel-chest'); // TODO: pending rename https://github.com/voxel/voxel-chest/issues/4
+  // Adds a hotbar for the player inventory
+  require('voxel-inventory-hotbar');  
+  // Adds 2x2 craftin area
   require('voxel-inventory-crafting');
-
+  // Displays name of the targeted block
   require('voxel-voila');
-
+  // Stores player health
   require('voxel-health');
-
+  // Shows player health
   require('voxel-health-bar');
-
+  // Adds food items which restore health
   require('voxel-food');
-
+  // Play sound effects on events
   require('voxel-sfx');
-
+  // Adds flight control
   require('voxel-flight');
-
+  // Toggle between creative and survival game mode
   require('voxel-gamemode');
-
+  //  Adds sprinting
   require('voxel-sprint');
-
+  // Adds textured planes on the side of blocks
   require('voxel-decals');
-
+  // Supports mining of blocks (essentially destroying them)
   require('voxel-mine');
-
+  // Works with voxel-mine to add an item representing the block in the players inventory
+  // Essentailly "harvesting" the block
   require('voxel-harvest');
-
+  // Allows interfacing with blocks and items
   require('voxel-use');
-
+  // Allows for raycasting and "getting" blocks
   require('voxel-reach');
-
+  // Adds picks, axes and spades
   require('voxel-pickaxe');
-
+  // Adds a hammer, similar to a pick but larger aoe
   require('voxel-hammer');
-
+  // Adds wool blocks
   require('voxel-wool');
-
-  require('voxel-pumpkin');
-
+  // Adds pumpkin block
+  require('voxel-pumpkin'); // TODO: pending rename https://github.com/voxel/voxel-pumpkin/issues/6
+  // Allows for adding additional data to blocks
   require('voxel-blockdata');
-
-  require('voxel-glass');
-
+  // Addds glass blocks
+  require('voxel-glass'); // TODO: pending rename https://github.com/voxel/voxel-glass/issues/6
+  // Terrain generator
   require('voxel-land');
-
-  require('voxel-decorative');
-
+  // Adds decorative blocks
+  require('voxel-decorative');  // TODO: pending rename https://github.com/voxel/voxel-decorative/issues/2
+  // Creative inventory dialog
   require('voxel-inventory-creative');
-
+  // Adds console widget
   require('voxel-console');
-
+  // Basic commands for voxel-console
   require('voxel-commands');
-
+  // Supports dropping several different files
   require('voxel-drop');
-
+  // Removes all distractions
   require('voxel-zen');
-
+  // Adjustable camera debug datgui settings for voxel-shader (voxel.js plugin)
   require('camera-debug');
-
+  // GUI for enabling and disabling plugins
   require('voxel-plugins-ui');
-
+  // Toggles fullscreen
   require('voxel-fullscreen');
-
+  // events for key bindings
   require('voxel-keys');
-
+  // ui for key bindings
   require('kb-bindings-ui');
 
   var createEngine = require('voxel-engine-stackgl');
