dbc91ef5c6d6f7de877d5c8011d4f6f09c8b0a9c
deathcap <deathcap@gmx.co.uk>
2016-05-09 21:51:22 -0700

    Update deploy to use local browserify

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 git checkout gh-pages
 git merge -X theirs master -m "Merge branch 'master' into gh-pages"
-browserify index.js > bundle.js
+./node_modules/.bin/browserify index.js > bundle.js
 git commit bundle.js -m "Regenerate bundle.js using deploy.sh"
 git checkout master
